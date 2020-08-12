0eea9a690e8dbddc5c64db519de543579675fd86
Suthan Bala <suthan.bb@gmail.com>
2020-08-12 11:01:03 -0400

    Resolved the following issues: Container names not correct when building the project, and the DB failing on the first build due to the build directory not found

--- scripts/export_db.sh
+++ scripts/export_db.sh
@@ -1,4 +1,9 @@
+if [ ! -d ./build ] 
+then
+    mkdir -p ./build
+fi
 echo "Please wait until we export your database...";
-docker exec mysql-wordpress bash -c 'mysqldump -u$DB_USER -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE > /db.sql; exit';
-docker cp mysql-wordpress:/db.sql ./build/db.sql
+container_name=$(docker ps --filter "ancestor=mysql" --format "{{.Names}}")
+docker exec $container_name bash -c 'mysqldump -u$DB_USER -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE > /db.sql; exit';
+docker cp $container_name:/db.sql ./build/db.sql
 echo "Database successfully copied."
--- scripts/export_src.sh
+++ scripts/export_src.sh
@@ -1,3 +1,8 @@
+if [ ! -d ./build ] 
+then
+    mkdir -p ./build
+fi
 echo "Please wait while we're exporting the WordPress files..."
-docker cp wp:/var/www/html/ ./build/
+container_name=$(docker ps --filter "ancestor=wordpress:latest" --format "{{.Names}}")
+docker cp $container_name:/var/www/html/ ./build
 echo "Successfully exported the files."
